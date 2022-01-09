142b9a8c1f61bec2f01702e5ba3a1eb79a471eda
zhengyx1982 <908788370@qq.com>
2022-01-09 11:52:06 +0800

    add image blur and canny

---
+++ blur and canny/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  testOpenCv
+//
+//  Created by nancy on 2022/1/3.
+//
+#include<opencv2/imgproc.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+using std::string;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    string path = "../resources/sunflower.png";
+    Mat image = imread(path); 
+    Mat imgBlur, imgCanny;
+    GaussianBlur(image, imgBlur, Size(9, 9), 10, 0);
+    Canny(image, imgCanny, 70, 30);
+    
+    imshow("origin", image);
+    imshow("blur", imgBlur);
+    imshow("canny", imgCanny);
+    waitKey(0);
+    
+    
+    return 0;
+}
